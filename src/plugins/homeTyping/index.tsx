8d0db2bd472313ca4f3ff2b19efc58f833f51c44
kucl8d <kxnmoap@gmail.com>
2026-04-17 08:05:33 +0300

    Update index.tsx

--- src/plugins/homeTyping/index.tsx
+++ src/plugins/homeTyping/index.tsx
@@ -533,7 +533,7 @@ function removeContainer() {
 export default definePlugin({
     name: "HomeTyping",
     description: "Changes the home button to a typing indicator if someone in your dms is typing, and shows typing notifications",
-    authors: [Devs.Savage, Devs.pluckerpilple],
+    authors: [Devs.pluckerpilple],
     settings,
 
     TypingIcon() {
