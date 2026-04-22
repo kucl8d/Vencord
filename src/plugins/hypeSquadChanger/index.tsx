fdfe0c2ea32e027e402d0063bda85cabcd9ab052
pluckerpilple <kxnmoap@gmail.com>
2026-04-22 06:56:48 +0300

    Update index.tsx

--- src/plugins/hypeSquadChanger/index.tsx
+++ src/plugins/hypeSquadChanger/index.tsx
@@ -283,7 +283,7 @@ function SettingsPanel() {
 export default definePlugin({
     name: "HypeSquadChanger",
     description: "Switch your Hypesquad house badge easily.",  
-    authors: [Devs.Savage,Devs.pluckerpilple],
+    authors: [Devs.pluckerpilple],
     settings,
     settingsAboutComponent: SettingsPanel,
 
