0fcb9422fc9acdbff6aeb4c85e280ed2855fc855
pluckerpilple <kxnmoap@gmail.com>
2026-04-29 17:53:08 +0300

    Update generateReport.ts

--- scripts/generateReport.ts
+++ scripts/generateReport.ts
@@ -20,7 +20,8 @@
 /// <reference types="../src/modules" />
 
 import { createHmac } from "crypto";
-import { readFileSync } from "fs";
+import { readFileSync, writeFileSync } from "fs";
+import { join } from "path";
 import pup, { JSHandle } from "puppeteer-core";
 
 const logStderr = (...data: any[]) => console.error(`${CANARY ? "CANARY" : "STABLE"} ---`, ...data);
@@ -41,7 +42,7 @@ let metaData = {
 const browser = await pup.launch({
     headless: true,
     executablePath: process.env.CHROMIUM_BIN,
-    args: ["--no-sandbox"]
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
 });
 
 const page = await browser.newPage();
@@ -80,20 +81,28 @@ const IGNORED_DISCORD_ERRORS = [
     "Downloading the full bad domains file",
     /\[GatewaySocket\].{0,110}Cannot access '/,
     "search for 'name' in undefined",
-    "Attempting to set fast connect zstd when unsupported"
+    "Attempting to set fast connect zstd when unsupported",
+    /Cannot read properties of undefined \(reading 'call'\)/,
+    "Could not complete Remote Auth login, trying to restart with a new Remote Auth session."
 ] as Array<string | RegExp>;
 
 function toCodeBlock(s: string, indentation = 0, isDiscord = false) {
     s = s.replace(/```/g, "`\u200B`\u200B`");
 
-    const indentationStr = Array(!isDiscord ? indentation : 0).fill(" ").join("");
-    return `\`\`\`\n${s.split("\n").map(s => indentationStr + s).join("\n")}\n${indentationStr}\`\`\``;
+    s = s.replace(/\\x([0-9A-Fa-f]{2})/g, (_, hex) =>
+        String.fromCharCode(parseInt(hex, 16))
+    );
+
+    const indentationStr = " ".repeat(!isDiscord ? indentation : 0);
+    const content = s.split("\n").map(s => indentationStr + s).join("\n");
+
+    return `\`\`\`\n${content}\n${indentationStr}\`\`\``;
 }
 
 async function printReport() {
     console.log();
 
-    console.log("# Vencord Report" + (CANARY ? " (Canary)" : ""));
+    console.log("# Equicord Report" + (CANARY ? " (Canary)" : ""));
 
     console.log();
 
@@ -188,13 +197,13 @@ async function printReport() {
         if (embeds.length === 1) {
             embeds.push({
                 title: "No issues found",
-                description: "Seems like everything is working fine (for now) <:shipit:1330992641466433556>",
+                description: "Seems like everything is working fine (for now)",
                 color: 0x00ff00
             });
         }
 
         const body = JSON.stringify({
-            username: "Vencord Reporter" + (CANARY ? " (Canary)" : ""),
+            username: "Equicord Reporter" + (CANARY ? " (Canary)" : ""),
             embeds
         });
 
@@ -241,7 +250,7 @@ page.on("console", async e => {
 
     const firstArg = await rawArgs[0]?.jsonValue();
 
-    const isVencord = firstArg === "[Vencord]";
+    const isEquicord = firstArg === "[Equicord]";
     const isDebug = firstArg === "[PUP_DEBUG]";
     const isReporterMeta = firstArg === "[REPORTER_META]";
 
@@ -251,7 +260,7 @@ page.on("console", async e => {
     }
 
     outer:
-    if (isVencord) {
+    if (isEquicord) {
         try {
             var args = await Promise.all(e.args().map(a => a.jsonValue()));
         } catch {
@@ -352,8 +361,8 @@ page.on("pageerror", (e: any) => {
 
 await page.evaluateOnNewDocument(`
     if (location.host.endsWith("discord.com")) {
-        ${readFileSync("./dist/browser.js", "utf-8")};
+        ${readFileSync("./dist/browser/browser.js", "utf-8")};
     }
 `);
 
-await page.goto(CANARY ? "https://canary.discord.com/login" : "https://discord.com/login");
+await page.goto(CANARY ? "https://canary.discord.com/login" : "https://discord.com/login", { timeout: 120000 });
