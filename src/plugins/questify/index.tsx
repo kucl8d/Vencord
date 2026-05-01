d03d976b42eb4b5c8af0e6fb720d611e83ef6c10
kucl8d <kxnmoap@gmail.com>
2026-05-01 19:18:53 +0300

    fix: src/plugins/questify/index.tsx

--- src/plugins/questify/index.tsx
+++ src/plugins/questify/index.tsx
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import "./styles.css";
+import "@plugins/questify/styles.css";
 
 import { showNotification } from "@api/Notifications";
 import { plugins } from "@api/PluginManager";
