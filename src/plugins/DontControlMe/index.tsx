146fc62a97cb5e11b8c01449cad37242b9606681
kucl8d <kxnmoap@gmail.com>
2026-05-02 04:44:14 +0300

    fix: src/plugins/DontControlMe/index.tsx

--- src/plugins/DontControlMe/index.tsx
+++ src/plugins/DontControlMe/index.tsx
@@ -189,7 +189,7 @@ function addPinChannelContextMenu(children: any, { channel }: { channel: any; })
 export default definePlugin({
     name: "DontControlMe",
     description: "Automatically reconnects, unmutes, undeafens, and prevents movement or disconnection from a pinned channel.",
-    authors: [Devs.Vich],
+    authors: [Devs.cute],
     isVich: true,
     settings,
 
